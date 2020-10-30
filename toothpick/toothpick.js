394259ce1ccef34b1a789e50efba2a33105ace5e
Lalit Umbarkar <lalit.umbarkar9@gmail.com>
2020-10-30 14:18:04 +0530

    Minor refactoring
    Signed-off-by: Lalit Umbarkar <lalit.umbarkar9@gmail.com>

--- toothpick/toothpick.js
+++ toothpick/toothpick.js
@@ -3,14 +3,47 @@
  * Created On : 11/10/20
  */
 
-const toothpick = (function () {
+const Utl = (function () {
 
     const oliveColor = "#b5cc18", blackColor = "#1b1c1d", greyColor = "#767676", pickWidth = 1, pickHeight = 100;
+
+    const rgbToHex = (r, g, b) => {
+        if (r > 255 || g > 255 || b > 255)
+            throw "Invalid color component";
+        return ((r << 16) | (g << 8) | b).toString(16);
+    }
+
+    return {
+        pickWidth, pickHeight,
+        getColorInd: (ctx, x, y) => {
+            let imageData = ctx.getImageData(x, y, 1, 1).data;
+            return "#" + ("000000" + rgbToHex(imageData[0], imageData[1], imageData[2])).slice(-6);
+        },
+        clearCanvas: (ctx) => {
+            ctx.clearRect(0, 0, ctx.canvas.clientWidth, ctx.canvas.clientHeight);
+            ctx.fillStyle = blackColor;
+            ctx.fillRect(0, 0, ctx.canvas.clientWidth, ctx.canvas.clientHeight);
+        },
+        drawVerticalToothpick: (ctx, cx, cy, latest) => {
+            ctx.fillStyle = oliveColor;
+            ctx.fillRect(cx, cy - (pickHeight / 2), pickWidth, pickHeight);
+            latest.push([cx, cy]);
+        },
+        drawHorizontalToothpick: (ctx, cx, cy, latest) => {
+            ctx.fillStyle = greyColor;
+            ctx.fillRect(cx - (pickHeight / 2), cy, pickHeight, pickWidth);
+            latest.push([cx, cy]);
+        }
+    }
+})();
+
+const toothpick = (function () {
+
     let ctx, visitedPicks = [], latestVertical = [], latestHorizontal = [];
 
     const addEdgeToothpicks = () => {
 
-        let halfDiff = pickHeight / 2,
+        let halfDiff = Utl.pickHeight / 2,
             previousVerticals = [...latestVertical],
             previousHorizontal = [...latestHorizontal];
         latestVertical = [];
@@ -20,50 +53,23 @@ const toothpick = (function () {
                 ex = eachPick[0], ey = eachPick[1],
                 abovePick = [ex, ey - halfDiff],
                 belowPick = [ex, ey + halfDiff];
+            // console.log("color", cHelper.getColorInd(ctx, cx - pickWidth, cy), cHelper.getColorInd(ctx, cx + pickWidth, cy));
             if (visitedPicks.indexOf(abovePick) === -1)
-                drawHorizontalToothpick(...abovePick);
+                Utl.drawHorizontalToothpick(ctx, ...abovePick, latestHorizontal);
             if (visitedPicks.indexOf(belowPick) === -1)
-                drawHorizontalToothpick(...belowPick);
+                Utl.drawHorizontalToothpick(ctx, ...belowPick, latestHorizontal);
         }
         while (previousHorizontal.length) {
             let eachPick = previousHorizontal.pop(),
                 ex = eachPick[0], ey = eachPick[1],
                 leftPick = [ex - halfDiff, ey],
                 rightPick = [ex + halfDiff, ey];
+            // console.log("color", cHelper.getColorInd(ctx, cx - pickWidth, cy), cHelper.getColorInd(ctx, cx + pickWidth, cy));
             if (visitedPicks.indexOf(leftPick) === -1)
-                drawVerticalToothpick(...leftPick);
+                Utl.drawVerticalToothpick(ctx, ...leftPick, latestVertical);
             if (visitedPicks.indexOf(rightPick) === -1)
-                drawVerticalToothpick(...rightPick);
+                Utl.drawVerticalToothpick(ctx, ...rightPick, latestVertical);
         }
-    };
-
-    const getColorInd = (x, y) => {
-        let imageData = ctx.getImageData(x, y, 1, 1).data;
-        return "#" + ("000000" + rgbToHex(imageData[0], imageData[1], imageData[2])).slice(-6);
-    }
-
-    const rgbToHex = (r, g, b) => {
-        if (r > 255 || g > 255 || b > 255)
-            throw "Invalid color component";
-        return ((r << 16) | (g << 8) | b).toString(16);
-    }
-
-    const drawVerticalToothpick = (cx, cy) => {
-        ctx.fillStyle = oliveColor;
-        console.log("vc", cx, cy, pickWidth, pickHeight,);
-        console.log("color", getColorInd(cx - pickWidth, cy), getColorInd(cx + pickWidth, cy));
-        ctx.fillRect(cx, cy - (pickHeight / 2), pickWidth, pickHeight);
-        latestVertical.push([cx, cy]);
-        visitedPicks.push([cx, cy]);
-    };
-
-    const drawHorizontalToothpick = (cx, cy) => {
-        ctx.fillStyle = greyColor;
-        console.log("hc", cx, cy, pickWidth, pickHeight);
-        console.log("color", getColorInd(cx, cy - pickWidth), getColorInd(cx, cy + pickWidth));
-        ctx.fillRect(cx - (pickHeight / 2), cy, pickHeight, pickWidth);
-        latestHorizontal.push([cx, cy]);
-        visitedPicks.push([cx, cy]);
     };
 
     const resetAndKeepOneToothpick = () => {
@@ -75,10 +81,8 @@ const toothpick = (function () {
         visitedPicks = [];
         latestVertical = [];
         latestHorizontal = [];
-        ctx.clearRect(0, 0, ctx.canvas.clientWidth, ctx.canvas.clientHeight);
-        ctx.fillStyle = blackColor;
-        ctx.fillRect(0, 0, ctx.canvas.clientWidth, ctx.canvas.clientHeight);
-        drawVerticalToothpick(ctx.canvas.clientWidth / 2, ctx.canvas.clientHeight / 2);
+        Utl.clearCanvas(ctx);
+        Utl.drawVerticalToothpick(ctx, ctx.canvas.clientWidth / 2, ctx.canvas.clientHeight / 2, latestVertical);
     };
 
     const registerListeners = () => {
